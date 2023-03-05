cf10f38da97a43c56ba86ca897a13841c8909942
cesarsiguencia <cesar.siguencia@gmail.com>
2023-03-04 21:26:13 -0500

    made minor changes to route layout

--- models/Comment.js
+++ models/Comment.js
@@ -7,34 +7,34 @@ Comment.init(
   {
     // columns will go here
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
     comment_text: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        validate: {
-            // this means the password must be at least four characters long
-            len: [1]
-          }
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+          len: [1]
+        }
     },
 
     user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
         model: 'user',
         key: 'id'
-        }
+      }
     },
     post_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
         model: 'post',
         key: 'id'
-        }
+      }
     }
   },
   {
--- models/index.js
+++ models/index.js
@@ -47,16 +47,16 @@ Comment.belongsTo(User, {
     foreignKey: 'user_id'
   });
   
-  Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
-  });
-  
-  User.hasMany(Comment, {
-    foreignKey: 'user_id'
-  });
-  
-  Post.hasMany(Comment, {
-    foreignKey: 'post_id'
-  });
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
 
 module.exports = { User, Post, Vote, Comment }
