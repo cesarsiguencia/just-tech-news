174dfca0472069867d2b2aac76b7ca56b43e57a6
cesarsiguencia <cesar.siguencia@gmail.com>
2023-03-17 10:46:26 -0400

    testing if staging is required

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -32,26 +32,6 @@ router.get('/', (req, res) => {
 });
 
 
-<<<<<<< HEAD
-router.get('/:id', withAuth, (req, res) => {
-    Post.findOne({
-        where: {
-        id: req.params.id
-        },
-        attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-        {
-            model: User,
-            attributes: ['username']
-=======
 router.get('/:id', (req, res) => {
   Post.findOne({
     where: {
