c219e8a567d4b09d3f797dfb391f22b0ed03ba7c
cesarsiguencia <cesar.siguencia@gmail.com>
2023-03-17 10:51:32 -0400

    .

--- models/Post.js
+++ models/Post.js
@@ -1,31 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 class Post extends Model {
-<<<<<<< HEAD
-    // previously this was in the post-routes.js file under posts/upvote PUT command. But it was complicated with two sequelize functions in it, so now it is being moved here so that VOTES is part of POSTS
-    static upvote(body, models) {
-        return models.Vote.create({
-            user_id: body.user_id,
-            post_id: body.post_id
-          }).then(() => {
-            return Post.findOne({
-              where: {
-                id: body.post_id
-              },
-              attributes: [
-                'id',
-                'post_url',
-                'title',
-                'created_at',
-                [
-                  sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-                  'vote_count'
-                ]
-              ]
-            });
-          });
-    }
-=======
   static upvote(body, models) {
     return models.Vote.create({
       user_id: body.user_id,
