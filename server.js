9fc7aceac796d6b0889328cd6aaedec584ebd110
cesarsiguencia <cesar.siguencia@gmail.com>
2023-04-25 18:38:33 -0400

    changing sequelize sync to force:true

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening on PORT 3320'));
 });
