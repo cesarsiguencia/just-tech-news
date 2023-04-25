44123f334bd14c21455faa0aaf5316f2c60e1459
cesarsiguencia <cesar.siguencia@gmail.com>
2023-04-25 15:44:49 -0400

    railway version with env upload

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,19 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-let sequelize;
+const sequelize = new Sequelize(
+  {     
+      
+      username: process.env.MYSQLUSER, 
+      password: process.env.MYSQLPASSWORD, 
+      database: process.env.MYSQLDATABASE,
+      dialect: 'mysql',
+      port: process.env.MYSQLPORT,
+      host: process.env.MYSQLHOST
+  }
+)
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
-}
+
+
 
 module.exports = sequelize;
-
